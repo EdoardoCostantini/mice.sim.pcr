092adc04a4cfc601e674dab39629bbfdd94976d8
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2021-07-29 11:14:14 +0200

    fix: durr.gaus reference and code style fixes

--- R/mice.impute.durr.gaus.R
+++ R/mice.impute.durr.gaus.R
@@ -10,14 +10,19 @@
 #' Draws a bootstrap sample from \code{x} and \code{y}, trains a
 #' penalised lasso regression and imputes with normal residuals.
 #' @author Edoardo Costantini, 2021
-#' @references Deng et. al. (2016)
+#' @references
+#'
+#' Deng, Y., Chang, C., Ido, M. S., & Long, Q. (2016). Multiple imputation for
+#' general missing data patterns in the presence of high-dimensional data.
+#' Scientific reports, 6(1), 1-10.
+#'
 #' @family univariate imputation functions
 #' @keywords datagen
 #' @export
 mice.impute.durr.gaus <- function(y, ry, x, wy = NULL, ...) {
   # Bootstrap sample
   if (is.null(wy)) wy <- !ry
-  idx_bs  <- sample(1:length(y), length(y), replace = TRUE)
+  idx_bs  <- sample(seq_along(y), length(y), replace = TRUE)
   y_star  <- y[idx_bs]
   ry_star <- ry[idx_bs]
   x_star <- x[idx_bs, ]
@@ -34,6 +39,6 @@ mice.impute.durr.gaus <- function(y, ry, x, wy = NULL, ...) {
                                 alpha = 1)
 
   # Obtain imputation
-  s2hat   <- mean((predict(cv_lasso, xobs) - yobs)**2)
+  s2hat   <- mean((predict(cv_lasso, xobs) - yobs)^2)
   as.vector(predict(cv_lasso, xmis, s = "lambda.min")) + rnorm(nrow(xmis), 0, sqrt(s2hat))
 }
